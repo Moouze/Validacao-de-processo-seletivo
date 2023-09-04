1024a337db6511fb1e23ecb1d017b5362f5320c6
Lucas <lucas_dev@outlook.com.br>
2023-09-04 15:32:24 -0300

    Solicitando as entradas para o usuário

--- src/AnalisandoCandidatos.java
+++ src/AnalisandoCandidatos.java
@@ -5,8 +5,31 @@ public class AnalisandoCandidatos {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Seja vem vindo a entrevista");
+        System.out.println("digite o salário pretendido: ");
+
+        double salario = sc.nextDouble();
+        sc.close();
+
+        analisarCandidato(salario);
 
 
+    }
 
+    static void analisarCandidato(double salarioPretendido) {
+
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+
+            System.out.println("Ligar para o candidato");
+
+        } else if (salarioBase == salarioPretendido) {
+
+            System.out.println("Ligar para o candidato com contra proposta");
+
+        } else
+
+            System.out.println("Aguardando o resultado dos demais candidatos");
     }
 }
